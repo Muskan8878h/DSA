4ae427b9235f475f9a55664c7edb24f44f3caea8
Muskan8878h <muskan8878h@gmail.com>
2025-10-30 09:52:36 +0530

    110. Balanced Binary Tree

---
+++ DSA Concepts/BinaryTree/Q7_110.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root)!= -1;        
+    }
+    private int dfs(TreeNode root){
+        if(root==null) return 0;
+        int leftHeight=dfs(root.left);
+        if(leftHeight==-1) return -1;
+        int rightHeight=dfs(root.right);
+        if(rightHeight==-1) return -1;
+        if(Math.abs(rightHeight-leftHeight)>1) return -1;
+        return Math.max(rightHeight,leftHeight)+1;
+    }
+}
