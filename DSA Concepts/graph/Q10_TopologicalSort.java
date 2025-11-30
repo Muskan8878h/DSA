36aca7a02093ceac433163e6fbd53b2df4a57539
Muskan8878h <muskan8878h@gmail.com>
2025-11-30 19:35:46 +0530

    Topological Sort GFG  using bfs

--- DSA Concepts/graph/Q10_TopologicalSort.java
+++ DSA Concepts/graph/Q10_TopologicalSort.java
@@ -36,3 +36,46 @@ class Solution {
         s.push(node);
     }
 }
+
+
+
+// bfs (Kahn's Algorithm)
+
+class Solution {
+    public ArrayList<Integer> topoSort(int V, int[][] edges) {
+        // code here
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] e: edges){
+            adj.get(e[0]).add(e[1]);
+        }
+        // indergee
+        int[] indegree=new int[V];
+        for(int i=0;i<V;i++){
+            for(int nei: adj.get(i)){
+                indegree[nei]++;
+            }
+        }
+        
+        
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0) q.offer(i);
+        }
+        
+        ArrayList<Integer> topo=new ArrayList<>();
+        // int idx=0;
+        for(int i=0;i<V;i++){
+            int node=q.poll();
+            topo.add(node);
+            for(int nei: adj.get(node)){
+                indegree[nei]--;
+                if(indegree[nei]==0) q.add(nei);
+            }
+        }
+        return topo;
+        
+    }
+}
