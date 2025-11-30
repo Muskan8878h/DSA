6607f1378661ecf898a09a270e484441f819c7c1
Muskan8878h <muskan8878h@gmail.com>
2025-11-30 16:06:22 +0530

    785. Is Graph Bipartite? using dfs

--- DSA Concepts/graph/Q8_785.java
+++ DSA Concepts/graph/Q8_785.java
@@ -22,3 +22,36 @@ class Solution {
         return true;
     }
 }
+
+
+// bfs
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n=graph.length;
+        int[] color=new int[n];
+        Arrays.fill(color,-1);
+
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                if(!bfs(graph,i,color,1)) return false;
+            }
+        }
+        return true;
+    }
+    private boolean bfs(int[][] graph, int start, int[] color, int curcol){
+        Queue<Integer> q=new LinkedList<>();
+        q.offer(start);  
+        color[start]=curcol;
+        while(!q.isEmpty()){
+            int node=q.poll();
+            for(int nei: graph[node]){
+                if(color[nei]==-1){
+                    color[nei]=1-color[node];
+                    q.offer(nei);
+                }
+                else if(color[nei]==color[node]) return false;
+            }
+        }
+        return true;
+    }
+} 
