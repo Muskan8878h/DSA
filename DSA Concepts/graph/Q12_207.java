9026a91ff5da004c47e2c7a9c099626bfd39eecb
Muskan8878h <muskan8878h@gmail.com>
2025-12-01 13:04:20 +0530

    207. Course Schedule using dfs

--- DSA Concepts/graph/Q12_207.java
+++ DSA Concepts/graph/Q12_207.java
@@ -28,3 +28,40 @@ class Solution {
         return count==numCourses;
     }
 }
+
+
+// dfs 
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] p: prerequisites){
+            adj.get(p[1]).add(p[0]);
+        }
+        boolean[] vis=new boolean[numCourses];
+        boolean[] rec=new boolean[numCourses];
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+                if(dfs(i,vis,rec,adj)) return false;
+            }
+        }
+        return true;
+    }
+    private boolean dfs(int node, boolean[] vis, boolean[] rec,List<List<Integer>> adj){
+        vis[node]=true;
+        rec[node]=true;
+        for(int nei: adj.get(node)){
+            if(!vis[nei]){
+                if(dfs(nei,vis,rec,adj)) return true;
+            }
+            else if(rec[nei]){
+                return true;
+            }
+        }
+        rec[node]=false;
+        return false;
+    }
+}
