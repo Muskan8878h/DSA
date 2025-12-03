7d2f54c04e260d0c971cb19b817785f2b5757419
Muskan8878h <muskan8878h@gmail.com>
2025-12-03 11:20:28 +0530

    3623. Count Number of Trapezoids I

--- DailyChallenge LeetCode/Q36_3623.java
+++ DailyChallenge LeetCode/Q36_3623.java
@@ -1,25 +1,18 @@
 class Solution {
-    int MOD=1_000_000_007;
+    int M=1_000_000_007;
     public int countTrapezoids(int[][] points) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int[] p: points){
-            map.put(p[1],map.getOrDefault(p[1],0)+1);
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int[] p : points) {
+            int y = p[1];
+            mp.put(y, mp.getOrDefault(y, 0) + 1);
         }
-        long sum=0;
-        long sumSq=0;
-        for(int cnt: map.values()){
-            if(cnt>=2){
-                long pairs=(long) cnt*(cnt-1)/2;
-                pairs%=MOD;
-                sum=(sum+pairs)%MOD;
-                sumSq=(sumSq+(pairs*pairs)%MOD)%MOD;
-            }
+        long result = 0;  
+        long prevHorizontalLines = 0;
+        for (int count : mp.values()) {
+            long horizontalLines = (long) count * (count - 1) / 2; 
+            result = (result + horizontalLines * prevHorizontalLines) % M;
+            prevHorizontalLines += horizontalLines;
         }
-        long ans=(sum*sum%MOD-sumSq+MOD)%MOD;
-        ans=ans*inv2()%MOD;
-        return (int)ans;
-    }
-    private long inv2(){
-        return (MOD+1)/2;
+        return (int) (result % M);
     }
 }
