76c563b9a05fe075b3cb425b7f9ef5c7c77fc976
Muskan8878h <149079628+Muskan8878h@users.noreply.github.com>
2025-12-22 11:06:20 +0530

    960. Delete Columns to Make Sorted III

---
+++ DailyChallenge LeetCode/Q56_960.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int n = strs.length;
+        int m = strs[0].length();
+
+        int[] dp = new int[m];
+        int maxKept = 1;
+        for (int j = 0; j < m; j++) {
+            dp[j] = 1;
+        }
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < j; i++) {
+                boolean valid = true;
+                for (int r = 0; r < n; r++) {
+                    if (strs[r].charAt(i) > strs[r].charAt(j)) {
+                        valid = false;
+                        break;
+                    }
+                }
+
+                if (valid) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                }
+            }
+            maxKept = Math.max(maxKept, dp[j]);
+        }
+
+        return m - maxKept;
+    }
+}
