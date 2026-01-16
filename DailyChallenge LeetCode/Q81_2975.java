6132fce1ff77404cc20766300c4924138a41a773
Muskan8878h <149079628+Muskan8878h@users.noreply.github.com>
2026-01-16 21:01:04 +0530

    2975. Maximum Square Area by Removing Fences From a Field

---
+++ DailyChallenge LeetCode/Q81_2975.java
@@ -0,0 +1,41 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+    public int maximizeSquareArea(int m, int n, int[] hFences, int[] vFences) {
+        int[] h = new int[hFences.length + 2];
+        int[] v = new int[vFences.length + 2];
+
+        h[0] = 1;
+        h[1] = m;
+        for (int i = 0; i < hFences.length; i++) {
+            h[i + 2] = hFences[i];
+        }
+
+        v[0] = 1;
+        v[1] = n;
+        for (int i = 0; i < vFences.length; i++) {
+            v[i + 2] = vFences[i];
+        }
+
+        Arrays.sort(h);
+        Arrays.sort(v);
+        Set<Long> heights = new HashSet<>();
+        for (int i = 0; i < h.length; i++) {
+            for (int j = i + 1; j < h.length; j++) {
+                heights.add((long) h[j] - h[i]);
+            }
+        }
+
+        long maxSide = -1;
+        for (int i = 0; i < v.length; i++) {
+            for (int j = i + 1; j < v.length; j++) {
+                long width = (long) v[j] - v[i];
+                if (heights.contains(width)) {
+                    maxSide = Math.max(maxSide, width);
+                }
+            }
+        }
+
+        if (maxSide == -1) return -1;
+        return (int) ((maxSide * maxSide) % MOD);
+    }
+}
