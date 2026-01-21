d6abc132c12061c62f63fa2dfaec18c2ba1f0cc6
Muskan8878h <149079628+Muskan8878h@users.noreply.github.com>
2026-01-21 10:18:49 +0530

    3315. Construct the Minimum Bitwise Array II

---
+++ DailyChallenge LeetCode/Q86_3315.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] result = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            if(nums.get(i)==2){
+                result[i]= -1;
+                continue;
+            }
+            boolean found = false;
+            for (int j = 1; j < 32; j++) {
+                if ((nums.get(i) & (1 << j)) > 0) {
+                    continue;
+                }
+                int prev = j - 1;
+                if (prev < 0) continue;
+                int x = nums.get(i) ^ (1 << prev);
+                result[i] = x;
+                found = true;
+                break;
+            }
+
+            if (!found) {
+                result[i] = -1;
+            }
+        }
+        return result;
+    }
+}
