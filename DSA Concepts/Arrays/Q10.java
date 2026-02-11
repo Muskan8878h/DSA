21985af0e5bf391ca0d260b4a8a7fccb04481062
Muskan8878h <149079628+Muskan8878h@users.noreply.github.com>
2026-02-11 11:53:58 +0530

    1572. Matrix Diagonal Sum

---
+++ DSA Concepts/Arrays/Q10.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int sum = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i==j || i+j == n-1){
+                    sum += mat[i][j];
+                }        
+            }
+        }
+        return sum;
+    }
+}
+
+// //// optimal-----------------------
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += mat[i][i];
+            sum += mat[i][n-i-1];
+        }
+        if(n%2 != 0){
+            sum -=mat[n/2][n/2];
+        }
+        return sum;
+    }
+}
