ec52f18c931683ae294fe8c3683a54818ff5edca
Muskan8878h <149079628+Muskan8878h@users.noreply.github.com>
2026-02-12 08:20:56 +0530

    3713. Longest Balanced Substring I

---
+++ DailyChallenge LeetCode/Q108_3713.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestBalanced(String s) {
+        int n = s.length();
+        int maxlen = 0;
+        for(int i=0; i<n; i++){
+            int[] freq = new int[26];
+            int distinct = 0;
+            int maxFreq = 0;
+            for(int j=i; j<n; j++){
+                int idx = s.charAt(j) - 'a';
+
+                if(freq[idx] == 0){
+                    distinct ++;
+                }
+                freq[idx] ++;
+                maxFreq = Math.max(maxFreq, freq[idx]);
+
+                int len = j-i+1;
+                if(len == distinct * maxFreq){
+                    maxlen = Math.max(maxlen, len);
+                }
+            }
+        }
+        return maxlen;
+    }
+}
