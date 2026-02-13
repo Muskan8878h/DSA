db3c1692fef669ed475e4d4ea1c12fe3b4ac82cd
Muskan8878h <149079628+Muskan8878h@users.noreply.github.com>
2026-02-13 11:26:54 +0530

    Maximum prefix sum for a given range

--- DSA Concepts/Arrays/Q25.java
+++ DSA Concepts/Arrays/Q25.java
@@ -2,16 +2,20 @@ class Solution {
     public List<Integer> maxPrefixes(List<Integer> arr, List<Integer> leftIndex, List<Integer> rightIndex) {
         // code here.
         List<Integer> res = new ArrayList<>();
-        int sum = 0;
-        int idx = 0;
-        int max = Integer.MIN_VALUE;
-        for(int i=leftIndex.get(idx); i<=rightIndex.get(idx); i++){
-            sum += arr.get(i);
-            max = Math.max(sum, max);
-            if(sum < 0){
-                sum = 0;
+        int q = leftIndex.size();
+        for(int j=0; j<q; j++){
+            int left = leftIndex.get(j);
+            int right = rightIndex.get(j);
+            
+            int sum = 0;
+            int max = Integer.MIN_VALUE;
+            
+            for(int i=left; i<=right; i++){
+                sum += arr.get(i);
+                max = Math.max(sum, max);
             }
+            res.add(max);
         }
-        return res.add(max);
+        return res;
     }
 }
