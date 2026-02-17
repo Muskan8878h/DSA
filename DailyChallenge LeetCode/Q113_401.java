1957086bb5abf3790ddfa8b02c9271fd51ec7bb7
Muskan8878h <149079628+Muskan8878h@users.noreply.github.com>
2026-02-17 14:08:02 +0530

    401. Binary Watch

---
+++ DailyChallenge LeetCode/Q113_401.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> res = new ArrayList<>();
+        for(int h = 0; h < 12; h++){
+            for(int m = 0; m < 60; m++){
+                int totalbit = Integer.bitCount(h) + Integer.bitCount(m);
+                if(totalbit == turnedOn){
+                    String time = h + ":" + (m<10 ? "0"+m : m);
+                    res.add(time);
+                }
+            }
+        }
+        return res;
+    }
+}
