286a8140459e5c5bd82a941731445e6899c00e3e
Muskan <149079628+Muskan8878h@users.noreply.github.com>
2026-02-22 08:06:41 +0530

    Q1. Find the Score Difference in a Game

---
+++ contest/8th/Q1. Find the Score Difference in a Game.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int scoreDifference(int[] nums) {
+        int first = 0;
+        int second = 0;
+        boolean firstActive = true;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]%2 != 0){
+                firstActive = !firstActive;
+            }
+            if(i%6 == 5){
+                firstActive = !firstActive;
+            }
+            if(firstActive){
+                first += nums[i];
+            }
+            else{
+                second += nums[i];
+            }
+        }
+        return first - second;
+    }
+}©leetcode
