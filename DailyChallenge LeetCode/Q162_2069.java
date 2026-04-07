b488495ededb2fe6bf55670c93f2046db6d6eba5
Muskan <149079628+Muskan8878h@users.noreply.github.com>
2026-04-07 20:33:19 +0530

    2069. Walking Robot Simulation II

---
+++ DailyChallenge LeetCode/Q162_2069.java
@@ -0,0 +1,43 @@
+class Robot {
+    int width, height;
+    int x, y;
+    int dir; 
+    int[] dx = {1, 0, -1, 0};
+    int[] dy = {0, 1, 0, -1};
+    String[] dirs = {"East", "North", "West", "South"};
+    int perimeter;
+    public Robot(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.x = 0;
+        this.y = 0;
+        this.dir = 0; 
+        this.perimeter = 2 * (width + height - 2);
+    }
+    public void step(int num) {
+        num = num % perimeter;
+        if (num == 0) {
+            if (x == 0 && y == 0) {
+                dir = 3;
+            }
+            return;
+        }
+        while (num > 0) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (nx < 0 || nx >= width || ny < 0 || ny >= height) {
+                dir = (dir + 1) % 4; 
+            } else {
+                x = nx;
+                y = ny;
+                num--;
+            }
+        }
+    }
+    public int[] getPos() {
+        return new int[]{x, y};
+    }
+    public String getDir() {
+        return dirs[dir];
+    }
+}
