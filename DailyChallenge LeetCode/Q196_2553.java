acb87a22b7c2d8a492032bb37deff27cb54ee0fa
Muskan <149079628+Muskan8878h@users.noreply.github.com>
2026-05-11 16:20:43 +0530

    2553. Separate the Digits in an Array

---
+++ DailyChallenge LeetCode/Q196_2553.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            String str = String.valueOf(num);
+
+            for (char ch : str.toCharArray()) {
+                list.add(ch - '0');
+            }
+        }
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+}
